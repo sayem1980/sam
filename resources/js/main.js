06d49dd6b625133fff5506324bf799808fa52fcc
sayem simanto <89852422+sayem1980@users.noreply.github.com>
2021-08-31 22:38:26 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+// Mixitup 
+
+var mixer = mixitup('.work-grid');
+
+// hamburger
+function openMenu() {
+    document.getElementById("hamburger").style.width ="100%";
+}
+function closeMenu() {
+    document.getElementById("hamburger").style.width ="0%";
+}
